4ea82507d9396231ec55a482abbba205b2f77f57
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-08-31 18:07:11 +0400

    fixed task 5

--- 05-react-jsx-green-button/script.js
+++ 05-react-jsx-green-button/script.js
@@ -1,16 +1,28 @@
-const App = () => {
-    let [buttonText, setButtonText] = React.useState('Click me');
+// App - функциональный компонент  с собственным состоянием
+const App = ({ initialButtonText, initialClassesList }) => {
+  // Метод объекта React - useState (возвращает массив из двух элементов) 
+  // - передаем начальное значение переменной
+  // setButtonText - это ф-ция и с помощью нее мы сможем указывать новое состояние buttonText
+  const [buttonText, setButtonText] = React.useState(initialButtonText);
+  const [classesList, setClassesList] = React.useState(initialClassesList);
 
-    const onButtonClick = () => {
-        buttonText = 'Hello from React';
-    };
+  const onButtonClick = () => {
+    setButtonText('Hello from React');  // меняем СОСТОЯНИЕ кнопки buttonText на другой текст
+    setClassesList('green-btn');
+  };
 
-    return (
-        <div className="app">
-            <button onClick={onButtonClick}>{buttonText}</button>
-        </div>
-      )
-}
-    const container = document.getElementById('app');
-    const root = ReactDOM.createRoot(container);
-    root.render(<App />);
+  // App возвращает JSX код
+  return ( 
+    // JSX компонент div имеет СВОЙСТВО className, JSX компонент button имеет СВОЙСТВО className и onClick
+    <div className="app"> 
+      <button className={classesList} onClick={onButtonClick}>{buttonText}</button> 
+    </div>
+  );
+};
+
+// Ф-ция App была вызвана дважды и вернула два разных результата 
+// (из-за внешней ф-ции React.useState("Click me")) - это ф-ция с побочными эффектами
+
+const container = document.getElementById("app");
+const root = ReactDOM.createRoot(container); // с помощью ReactDOM мы показываем компонент App на странице
+root.render(<App initialButtonText="Click me" initialClassesList=""/>); // рендерим компонент в определнной части страницы
