88ec7484a4abee6f0db894df42afb11d217bc6e4
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-09-06 17:12:34 +0400

    added task 10 - conditionals

---
+++ 10-conditional-rendering/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import PetInfo from './components/PetInfo';
+
+function App() {
+  return (
+    <div className="App">
+      { <PetInfo animal="dog" age={2} hasPet/>}
+      { <PetInfo animal="cat" age={7} hasPet={false}/>}
+    </div> 
+  );
+}
+
+export default App;
