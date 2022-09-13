f426f5590b4734b4dd08b419c1e042b3f7784188
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-09-13 12:11:25 +0400

    added task 13

---
+++ 13-map/src/App.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import './App.css';
+import Button from './components/Button';
+import Counter from './components/Counter';
+
+const texts = [
+  'Click me',
+  'Click me, please',
+  'Hit me',
+  'Press me',
+]
+
+function App() {
+  const [count, setCount] = useState(0);
+  const incrementCount = () => {
+    setCount(count + 1);
+  }
+
+  return (
+    <div className="App">
+      <Counter count={count}/>
+      {texts.map((text, index) => {
+        return <Button onClick={incrementCount} text={text} key={index}/>
+      })}
+    </div>
+  );
+}
+
+export default App;
