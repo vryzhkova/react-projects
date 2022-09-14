c5e1f096218cf603c6ae15558c4422dc27aee028
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-09-14 15:26:41 +0400

    added task16

---
+++ 16-react-fragment/src/components/Card.js
@@ -0,0 +1,11 @@
+const Card = () => {
+    return (
+        <>
+            <h1>Vika</h1>
+            <h3>I am recording React course</h3>
+            <button>Like!</button>
+        </>
+    )
+}
+
+export default Card;
