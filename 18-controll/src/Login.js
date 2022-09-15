e46002bf7e4b5b79c1df723b78b669ac2a72debd
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-09-15 12:27:49 +0400

    added task 18

---
+++ 18-controll/src/Login.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+function Login() {
+    const [data, setData] = useState({username: '', password: ''});
+
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        console.log(data);
+        alert(JSON.stringify(data));
+    }
+
+    function handleInputChange (e, name) {
+        setData({...data, [name]: e.target.value})
+    }
+
+    return (
+        <>
+            <h1>Login form</h1>
+            <form onSubmit={handleFormSubmit}>
+                <label>Username:
+                    <input type="text" value={data.username}
+                     onChange={(e) => handleInputChange(e, 'username')}/>
+                </label>
+                <label>Password:
+                    <input type="password" value={data.password}
+                     onChange={(e) => handleInputChange(e, 'password')}/>
+                </label>
+                <button type="submit">Login</button>
+            </form>
+        </>
+    )
+}
+
+export default Login;
